4ec920d1883a7bf3a1715a508eef46e14909466a
Serhii Yasenev <serhii.yasenev@gmail.com>
2025-11-04 19:05:25 +0200

    lesson 9 hometask  (#17)
    * upd
    
    * hometask 9

---
+++ lesson-9-hometask/my-project/src/types.ts
@@ -0,0 +1,12 @@
+export type Status = 'todo' | 'in_progress' | 'done';
+export type Priority = 'low' | 'medium' | 'high';
+
+export interface Task {
+    id: string;
+    title: string;
+    description: string;
+    status: Status;
+    priority: Priority;
+    createdAt: Date;
+    deadline?: Date | null;
+}
