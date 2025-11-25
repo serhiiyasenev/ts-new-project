01bed9707957678b791e77b00e9564207acb7270
serhiiyasenev <serhii.yasenev@gmail.com>
2025-11-25 23:36:18 +0200

    fix PR comments

--- lesson-11/scripts/openCoverage.js
+++ lesson-11/scripts/openCoverage.js
@@ -1,16 +1,14 @@
 const { exec } = require("child_process");
 const path = require("path");
 
-// абсолютний шлях до coverage/index.html
 const coverageFile = path.join(__dirname, "..", "coverage", "lcov-report", "index.html");
 
-// Команда для Windows
 const command = `start "" "${coverageFile.replace(/\\/g, "\\\\")}"`;
 
 exec(command, (err) => {
   if (err) {
     console.error("Failed to open coverage:", err);
   } else {
-    console.log("📊 Coverage opened:", coverageFile);
+    console.log("Coverage opened:", coverageFile);
   }
 });
