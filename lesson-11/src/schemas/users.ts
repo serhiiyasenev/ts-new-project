fa4982e7da8a96de14095966f51e920a1e1ba6c3
Serhii Yasenev <serhii.yasenev@gmail.com>
2025-11-25 23:19:27 +0200

    Update lesson-11/src/schemas/users.ts
    Co-authored-by: Igor Postrygan <igorpostrygan@gmail.com>

--- lesson-11/src/schemas/users.ts
+++ lesson-11/src/schemas/users.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const createUserSchema = z.object({
   name: z.string().min(2, "Name must have at least 2 characters"),
-  email: z.string().email("Email must be valid"),
+  email: z.email("Email must be valid"),
   isActive: z.boolean().optional()
 });
 
